85ab97f7b2072a363a156889f235f7b9e32c1873
Sergei Grechanik <sergei.grechanik@gmail.com>
2013-03-20 16:49:02 +0400

    Samples; EqProver seems to work

--- README.md
+++ README.md
@@ -7,6 +7,30 @@ currently limited to a lazy first-order functional language.
 It also contains an experimental supercompiler that can prove equivalence of
 functions.
 
+## Experimental equivalence prover
 
+Hypergraph-based supercompilation is good at proving equivalences.
+To demonstrate this the project contains an equivalence prover. 
+To try it out you should first build the project and create a start script using sbt:
 
+    sbt start-script
+
+This command will create a script `./target/start` which runs the equivalence prover.
+The equivalence prover should be provided with a file containing definitions and (optionally)
+a task in the form `foo=bar`:
+
+    ./target/start -t constz=idle samples/idle
+
+If the first line of the file contains a comment with a task (in the same format) then you can pass 
+`auto` instead:
+
+    ./target/start -tauto samples/idle
+
+You can use the flag `-v` to make the prover more verbose (for example, 
+it will then print what expressions it is merging by graph isomorphism).
+Note also that some examples may need adjustment of parameters:
+
+    ./target/start -tauto -a4 -v  samples/add-assoc
+
+(`-a4` increases the arity limit to 4)
 
--- src/main/scala/graphsc/app/EqProverApp.scala
+++ src/main/scala/graphsc/app/EqProverApp.scala
@@ -12,11 +12,14 @@ object EqProverApp {
   class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
     version("Equivalence prover based on hypergraph supercompilation, version 0.0")
     banner("Usage: EqProver [OPTIONS] file")
-    //val task = opt[String]()
+    val task = opt[String](descr = 
+      "An equivalence we want to prove (up to renaming!) of the form foo=bar " +
+    		"or auto to read the task from the first line of the file")
     val arity = opt[Int](default = Some(3), descr = "Maximal arity of nodes")
     val depth = opt[Int](default = Some(3), descr = "Depth limit")
     val codepth = opt[Int](default = Some(3), descr = "Codepth limit")
     val nogen = opt[Boolean](noshort = true, descr = "Disable generalization")
+    val noiso = opt[Boolean](noshort = true, descr = "Disable merging by isomorphism")
     val generations = opt[Int](default = Some(1000), descr = "Maximal number of generations")
     val dumpDot = opt[Boolean](noshort = true, descr = "Dump the graph to stdout")
     val verbose = opt[Boolean](descr = "Be more verbose")
@@ -48,6 +51,27 @@ object EqProverApp {
     src.close()
     parser(srctext)
     
+    // get the task
+    val task =
+      conf.task.get.map{ s => 
+        if(s == "auto") {
+          val m = "--[\t ]*([^= \t\n\r\f]+)=([^= \t\n\r\f]+)".r.findPrefixMatchOf(srctext)
+          if(!m.isDefined) {
+            System.err.println("Cannot read a task from file")
+            System.exit(1)
+          }
+          (graph(m.get.group(1)), graph(m.get.group(2))) 
+        }
+        else {
+          val names = s.split("=")
+          if(names.length != 2) {
+            System.err.println("Invalid task format: " + s)
+            System.exit(1)
+          }
+          (graph(names(0)), graph(names(1)))
+        }
+      }
+    
     // assign zero (co)depth to all initial nodes
     for(n <- graph.allNodes)
       graph.zeroBoth(n.deref)
@@ -71,10 +95,22 @@ object EqProverApp {
       }
     }
     
+    def checktask() : Boolean = {
+      for((l,r) <- task)
+        if(l.deref.node == r.deref.node) {
+          stop = true
+          return true
+        }
+      false
+    }
+    
     stats()
+    checktask()
     
     // main loop
     while(!stop && generation < conf.generations.get.get) {
+      if(conf.verbose.isSupplied)
+          System.err.println("Transforming...")
       while(graph.updatedHyperedges.nonEmpty) {
         val trans =
           if(conf.nogen.isSupplied) tr.transDrive
@@ -88,10 +124,76 @@ object EqProverApp {
       
       generation += 1
       
+      checktask()
+      
+      if(!stop && !conf.noiso.isSupplied) {
+        if(conf.verbose.isSupplied)
+          System.err.println("Computing likeness...")
+        val nodes = graph.allNodes.toList
+        val like =
+          for(l <- nodes; r <- nodes; if l != r && l.hashCode <= r.hashCode; 
+              lkl <- LikenessCalculator[Int].likenessN(l, r); if lkl._1 > 0) yield {
+            (lkl,l,r)
+          }
+        
+        var eprover = new EquivalenceProver(graph)
+        
+        if(conf.verbose.isSupplied)
+          System.err.println("Performing merging by isomorphism...")
+        for(((i,ren),l,r) <- like.toList.sortBy(-_._1._1)
+              if i > 0 && l.deref.node != r.deref.node) {
+          val lpretty = l.prettyDebug
+          val rpretty = r.prettyDebug
+          val eq = eprover.prove(l.deref.node, r.deref.node)
+          if(eq != None) {
+            if(conf.verbose.isSupplied) {
+              System.err.println("==These two are equal==")
+              System.err.println(lpretty)
+              System.err.println("=======================")
+              System.err.println(rpretty)
+              System.err.println("=======================\n")
+              //System.err.println(eq)
+            }
+            eq.get.performGluing(graph)
+            val st = eprover.stats
+            eprover = new EquivalenceProver(graph)
+            eprover.stats = st
+          }
+        }
+        
+        /*
+        val stats = eprover.stats
+        
+        for(((l1,r1),c) <- stats.toList.sortBy(_._2)) {
+          val l = l1.deref.node
+          val r = r1.deref.node
+          val lik = LikenessCalculator[Int].likeness(l.deref, r.deref)
+          if(lik != None && lik.get._1 == 0 ) {
+            println(c + " $$$$$$$ This would help $$$$$$$$$ " + lik)
+            println(l.prettyDebug)
+            println("---")
+            println(r.prettyDebug)
+            println("^^^^^^^^^^")
+            g.zeroBoth(l.deref)
+            g.zeroBoth(r.deref)
+            if(g.drive(l) == None)
+              println("seems undrivable\n" + l.prettyDebug)
+            if(g.drive(r) == None)
+              println("seems undrivable\n" + r.prettyDebug)
+          }
+        }*/
+      }
+      
       stats()
       
+      checktask()
       
     }
+    
+    if(checktask())
+      System.err.println("The equivalence was successfully proved (up to renaming)")
+    else if(conf.task.isSupplied)
+      System.err.println("I was unable to prove the equivalence")
     
     if(conf.dumpDot.isSupplied) {
       println(graph.toDot)
